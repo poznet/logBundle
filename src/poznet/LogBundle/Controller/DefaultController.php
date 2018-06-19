9d415e33bf9c9f683e5104e8bf8019784bc67ab5
Michał Glajc <pozycjoner.net@gmail.com>
2018-06-19 09:49:01 +0200

    initial bundle

---
+++ src/poznet/LogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace poznet\LogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('poznetLogBundle:Default:index.html.twig');
+    }
+}
---
+++ src/poznet/LogBundle/poznetLogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace poznet\LogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class poznetLogBundle extends Bundle
+{
+}
